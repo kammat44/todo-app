5784983dd346d1ed9958fe432d7561c3bcc76cce
GwiezdnyRycerzKosmosu <m.kaminski.gik@gmail.com>
2022-07-26 17:14:46 +0200

    Add db migration

--- src/main/java/com/udemyspring/todoapp/controller/TaskController.java
+++ src/main/java/com/udemyspring/todoapp/controller/TaskController.java
@@ -6,11 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 class TaskController {
